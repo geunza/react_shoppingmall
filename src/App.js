e7453695dfd16a653543f04a3670cc8236a233c1
geunza <65527146+geunza@users.noreply.github.com>
2022-06-24 16:57:47 +0900

    shoes state 작성 및 컴포넌트화

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button, Navbar, Container, Nav} from 'react-bootstrap'
+import bg from './bg.png'
+import data from './data.js'
+import { useState } from 'react';
 
 function App() {
+  let [shoes, setShoes] = useState(data);
+  let [shoes_title, setShoes_title] = useState([]);
+  let [shoes_price, setShoes_price] = useState([]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="main-bg"></div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
+      <div>
+        
+      </div>
+      <div className="container">
+        <div className="row">
+          {
+            shoes.map(function(v, i, arr){
+              return <Card v={v} i={i}/>
+            })
+          }
+        </div>
+      </div>
     </div>
   );
 }
-
+function Card(props){
+  return(
+    <div className="col-md-4">
+      <img src={`https://codingapple1.github.io/shop/shoes${props.i+1}.jpg`} width="80%" />
+      <h4>{props.v.title}</h4>
+      <p>{props.v.price}</p>
+    </div>
+  )
+}
 export default App;
