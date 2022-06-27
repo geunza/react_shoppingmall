012391552cf89d7388d5ebef2a4d36980cf63e83
geunza <65527146+geunza@users.noreply.github.com>
2022-06-27 18:05:35 +0900

    탭기능 추가

--- src/routes/detail.js
+++ src/routes/detail.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom'
+import {Button, Navbar, Container, Nav} from 'react-bootstrap'
 import styled from 'styled-components'
 
 // class Detail2 extends react.Component{
@@ -20,23 +21,25 @@ function Detail(props){
   let [count, setCount] = useState(0);
   let [box, setBox] = useState(true);
   let [num, setNum] = useState(0);
+  let [tab, setTab] = useState(0);
   id = parseInt(id);
   let item = props.shoes.find(function(x){
     return x.id == id;
   });
 
   useEffect( ()=>{ //mount, update시 실행
-    let timer = setTimeout(()=>{
-      setBox(!box);
-    }, 2000);
+    //let timer = setTimeout(()=>{
+    //  setBox(!box);
+    //}, 2000);
+    console.log(tab);
     if(isNaN(num) == true){
       alert('그러지마세요')
       
     }
     return ()=>{
-      clearTimeout(timer);
+      //clearTimeout(timer);
     }
-  }, [box, num] );
+  }, [box, num, tab] );
 
   return(
     <div className="container">
@@ -64,9 +67,33 @@ function Detail(props){
           <button className="btn btn-danger">주문하기</button> 
         </div>
       </div>
+      <Nav variant="tabs"  defaultActiveKey="link0">
+        <Nav.Item>
+          <Nav.Link eventKey="link0" onClick={ ()=>{setTab(0)} }>버튼0</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link1" onClick={ ()=>{setTab(1)} }>버튼1</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link2" onClick={ ()=>{setTab(2)} }>버튼2</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <div className="tabArea">
+        <TabContent tab={tab}/>
+      </div>
+
     </div> 
   )
 }
-
+function TabContent(props){
+  return [<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][props.tab];
+  // if(props.tab == 0){
+  //   return <div>내용0</div>
+  // }else if(props.tab == 1){
+  //   return <div>내용1</div>
+  // }else if(props.tab == 2){
+  //   return <div>내용2</div>
+  // }
+}
 
 export default Detail;
